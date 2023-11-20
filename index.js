3737682cdae2cb85565d1d6351b96fb86cd21e47
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-20 13:39:13 -0500

    show all games

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
